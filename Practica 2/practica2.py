ca58bbde0ae6556d532bb29916e5fe11ed9622d8
Jeal <jesusagustiniano@gmail.com>
2023-10-07 11:10:18 +0200

    ParteCsv

--- Practica 2/practica2.py
+++ Practica 2/practica2.py
@@ -18,10 +18,29 @@ def accidentes_por_distrito_tipo(datos):
         result[tupla] = lista.count(tupla)
 
     pprint(result)
+    return result
+
+
+def puntos_negros_distrito(datos, distrito, k):
+    distritoList = []
+    result = []
+    distritoList = [x for x in datos if x.get('distrito') == distrito]
+    localidades = [x.get('localizacion') for x in distritoList]
+
+    for linea in distritoList:
+        local = linea.get('localizacion')
+        tupla = (local, localidades.count(local))
+        if tupla not in result:
+            result.append(tupla)
+
+    result.sort(key=lambda x: (x[1], x[0]), reverse=True)
+    result = result[:k]
+    pprint(result)
 
 
 with open("AccidentesBicicletas_2021.csv", 'r', newline='', encoding='utf8') as file:
     datos = csv.DictReader(file, delimiter=';')
     lista = list(datos)
-    accidentes_por_distrito_tipo(lista)
+    # accidentes_por_distrito_tipo(lista)
+    puntos_negros_distrito(lista, 'MONCLOA-ARAVACA', 16)
     # pprint(lista[:10])
