a8046eaf8fba17bb6a5fd8c0e0e7da7dcc9bf461
WhiteMoon-Knight <josejaca@ucm.es>
2023-10-14 11:18:24 +0200

    sigue sin funcionar

--- Practica 3/XML_2.py
+++ Practica 3/XML_2.py
@@ -1,3 +1,4 @@
+import pprint
 import xml.sax
 
 def subcategorias(filename):
@@ -14,9 +15,13 @@ def subcategorias(filename):
 
         def startElement(self, name, attrs):
             self.curr_path.append(name)
+            
+            
             if name == "item":
-                if attrs["name"] == "idCategoria" and attrs["name"] == "idSubcategoria":
-                    self.curr_path.pop()
+                if attrs["name"] != "idCategoria" or attrs["name"] != "idSubCategoria":
+                    self.curr_path.append(name)
+            else:
+                self.curr_path.append(name)
 
         def endElement(self, name):
             self.curr_path.pop()
@@ -24,7 +29,7 @@ def subcategorias(filename):
         def characters(self, content):
             if len(self.curr_path) >= 2 and self.curr_path[-2:] == ["categoria","item"]:
                 self.categoria = content
-            if len(self.curr_path) >= 3 and self.curr_path[-4:] == ["categoria","subcategorias","subcategoria","item"]:
+            if len(self.curr_path) >= 4 and self.curr_path[-4:] == ["categoria","subcategorias","subcategoria","item"]:
                 self.subcategoria = content
                 self.lista.append((self.categoria,self.subcategoria))
     
@@ -37,5 +42,6 @@ def subcategorias(filename):
     return Ejercicio2Handler.lista
 
 
-print(subcategorias("restaurantes_v1_es_pretty.xml"))
+for i in subcategorias("restaurantes_v1_es_pretty.xml"):
+    print(i)
 
