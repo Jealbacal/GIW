919e1765b4922d3f115ef5db8351857ec9f8bdde
PoloDeNaranja <enrima08@ucm.es>
2023-10-22 16:22:36 +0200

    Create formato_xml.py

---
+++ Practica 3/formato_xml.py
@@ -0,0 +1,28 @@
+# TODO: rellenar
+# Asignatura: GIW
+# Práctica XXXXXX
+# Grupo: XXXXXXX
+# Autores: XXXXXX
+#
+# Declaramos que esta solución es fruto exclusivamente de nuestro trabajo personal. No hemos
+# sido ayudados por ninguna otra persona o sistema automático ni hemos obtenido la solución
+# de fuentes externas, y tampoco hemos compartido nuestra solución con otras personas
+# de manera directa o indirecta. Declaramos además que no hemos realizado de manera
+# deshonesta ninguna otra actividad que pueda mejorar nuestros resultados ni perjudicar los
+# resultados de los demás.
+
+
+def nombres_restaurantes(filename):
+    ...
+
+
+def subcategorias(filename):
+    ...
+
+
+def info_restaurante(filename, name):
+    ...
+
+
+def busqueda_cercania(filename, lugar, n):
+    ...
