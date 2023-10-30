9f1618975544c700e4d2ecd49e3053e05dc720b0
PoloDeNaranja <enrima08@ucm.es>
2023-10-30 17:18:59 +0100

    Ej1 de la pracitca 5 hecho pero hay que hacer la practica 4 antes jeje

---
+++ Practica 4/bd.py
@@ -0,0 +1,39 @@
+"""
+TODO: rellenar
+
+Asignatura: GIW
+Práctica 4
+Grupo: XXXXXXX
+Autores: XXXXXX 
+
+Declaramos que esta solución es fruto exclusivamente de nuestro trabajo personal. No hemos
+sido ayudados por ninguna otra persona o sistema automático ni hemos obtenido la solución
+de fuentes externas, y tampoco hemos compartido nuestra solución con otras personas
+de manera directa o indirecta. Declaramos además que no hemos realizado de manera
+deshonesta ninguna otra actividad que pueda mejorar nuestros resultados ni perjudicar los
+resultados de los demás.
+"""
+
+
+def crear_bd(db_filename):
+    ...
+
+
+def cargar_bd(db_filename, tab1, tab2, tab3):
+    ...
+
+
+def consulta1(db_filename, limite):
+    ...
+
+
+def consulta2(db_filename):
+    ...
+
+
+def consulta3(db_filename, limite):
+    ...
+
+
+def consulta4(db_filename, ticker):s
+    ...
