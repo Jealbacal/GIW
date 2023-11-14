d3db05c419a209d24bfe2cf7ae03a917cac11f85
WhiteMoon-Knight <josejaca@ucm.es>
2023-11-14 18:56:51 +0100

    ej 1 sin funcionar, mañana le meto mas

--- Practica 6/uso_apis.py
+++ Practica 6/uso_apis.py
@@ -14,11 +14,25 @@ deshonesta ninguna otra actividad que pueda mejorar nuestros resultados ni perju
 resultados de los demás.
 """
 
+#----TOKEN---------------------------------------------------------
+# 01ddb601641b1808561a2a70a35c5f5c6d4562b56c7379defdd216d81b0f0b39
+#------------------------------------------------------------------
+
+import requests
+from pprint import pprint
 
 #Ejercicio 1
 
 def inserta_usuarios(datos, token):
-    ...
+
+    for d in datos:
+        r = requests.post('https://gorest.co.in/public/v2/users',
+                    headers={'Authorization': f'Bearer {token}'},
+                    data = d
+                    )
+        if (r.status_code % 100 != 2): return False
+
+    return True
 
 #Ejercicio 2
 
@@ -44,3 +58,15 @@ def lista_todos(email, token):
 
 def lista_todos_no_cumplidos(email, token):
     ...
+
+# Testeo
+with open('token_gorest.txt','r',encoding='utf8') as f:
+    TOKEN_GOREST = f.read().strip()
+
+    #Teste ej 1
+    u1 = {'name': 'Eva', 'email': 'eva@gmail.com', 'gender': 'female', 'status': 'inactive'}
+    u2 = {'name': 'Ana', 'email': 'ana@gmail.com', 'gender': 'female', 'status': 'active'}
+    u3 = {'name': 'Pepe', 'email': 'pepe@gmail.com', 'gender': 'female', 'status': 'inactive'}
+
+    print(inserta_usuarios([u1,u2], TOKEN_GOREST))
+    print(inserta_usuarios([u2,u3], TOKEN_GOREST))
